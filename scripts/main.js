8734d1d2ee27f62d8dfacbdb5de57968ca71dd07
shinde ajay <shindeajayb@outlook.com>
2020-07-06 16:25:18 +0530

    Add change user

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,27 @@ myImage.onclick = function(){
         myImage.setAttribute('src','images/mozilla-logo.png');
     }
 }
+
+//change-user-button-onclick
+
+let userBtn = document.querySelector('.user-btn');
+let mainHeading = document.querySelector('.header');
+
+userBtn.onclick = function(){
+    setUserName();
+}
+function setUserName(){
+    let userName = prompt('Enter your name.');
+    if(!userName){
+        setUserName();
+    }else{
+        localStorage.setItem('username',userName);
+        mainHeading.textContent = 'Mozilla is cool, ' + userName; 
+    }
+}
+if(!localStorage.getItem('username')){
+    setUserName();
+}else{
+    let storedUserName = localStorage.getItem('username');
+    mainHeading.textContent = 'Mozilla is cool, ' + storedUserName;
+}
